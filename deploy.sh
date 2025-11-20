cb67fad45dc7a9763ccdabc193cbe5083e263a02
KomarovAI <artur.komarovv@gmail.com>
2025-11-21 02:29:21 +0300

    chore: Remove deploy scripts (deployment via FluxCD)

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-kubectl apply -f manifests/namespace.yaml
-kubectl apply -f manifests/storageclass.yaml
-kubectl apply -f manifests/persistentvolume.yaml
-kubectl apply -f manifests/persistentvolumeclaim.yaml
-kubectl apply -f manifests/statefulset.yaml
