a05fd21aaa575a51080e31cf05d9524fb450e0fd
KomarovAI <artur.komarovv@gmail.com>
2025-11-21 02:30:09 +0300

    chore: Remove uninstall.sh (deployment via FluxCD)

--- uninstall.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-kubectl delete -f manifests/statefulset.yaml --ignore-not-found
-test -f manifests/persistentvolumeclaim.yaml && kubectl delete -f manifests/persistentvolumeclaim.yaml --ignore-not-found
-test -f manifests/persistentvolume.yaml && kubectl delete -f manifests/persistentvolume.yaml --ignore-not-found
-test -f manifests/storageclass.yaml && kubectl delete -f manifests/storageclass.yaml --ignore-not-found
-test -f manifests/namespace.yaml && kubectl delete -f manifests/namespace.yaml --ignore-not-found
